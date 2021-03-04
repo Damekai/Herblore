37aaca3efacfed7ea8f5201bc904f1107f046c3e
Damekai <damekaidev@gmail.com>
2021-03-03 23:02:33 -0500

    Fixed toxicity tier display in vanilla Effects GUI.

--- src/main/java/com/damekai/herblore/common/capability/ToxicityHandler.java
+++ src/main/java/com/damekai/herblore/common/capability/ToxicityHandler.java
@@ -15,11 +15,13 @@ public class ToxicityHandler implements IToxicityHandler
     private static final int TOXICITY_DECAY_RATE = 60; // Number of ticks required for toxicity to decrease by 1.
 
     private int toxicity;
+    private int toxicityTier;
     private int ticksSinceLastDecay;
 
     public ToxicityHandler()
     {
         toxicity = 0;
+        toxicityTier = 0;
         ticksSinceLastDecay = 0;
     }
 
@@ -27,21 +29,21 @@ public class ToxicityHandler implements IToxicityHandler
     public void addToxicity(LivingEntity livingEntity, int amount)
     {
         toxicity += amount;
-        updateToxicityEffect(livingEntity);
+        onToxicityChange(livingEntity);
     }
 
     @Override
     public void removeToxicity(LivingEntity livingEntity, int amount)
     {
         toxicity -= amount;
-        updateToxicityEffect(livingEntity);
+        onToxicityChange(livingEntity);
     }
 
     @Override
     public void setToxicity(LivingEntity livingEntity, int amount)
     {
         toxicity = Math.max(amount, 0);
-        updateToxicityEffect(livingEntity);
+        onToxicityChange(livingEntity);
     }
 
     @Override
@@ -55,24 +57,7 @@ public class ToxicityHandler implements IToxicityHandler
     {
         toxicity = 0;
         ticksSinceLastDecay = 0;
-        updateToxicityEffect(livingEntity);
-    }
-
-    public void tickToxicityDecay(LivingEntity livingEntity)
-    {
-        if (toxicity > 0)
-        {
-            if (ticksSinceLastDecay == TOXICITY_DECAY_RATE)
-            {
-                toxicity--;
-                ticksSinceLastDecay = 0;
-                updateToxicityEffect(livingEntity);
-            }
-            else
-            {
-                ticksSinceLastDecay++;
-            }
-        }
+        onToxicityChange(livingEntity);
     }
 
     public CompoundNBT serializeNBT()
@@ -85,21 +70,52 @@ public class ToxicityHandler implements IToxicityHandler
         // TODO
     }
 
-    private void updateToxicityEffect(LivingEntity livingEntity)
+    /**
+     * @return True if the toxicity tier was changed.
+     */
+    private boolean updateToxicityTier()
+    {
+        int updatedToxicityTier = toxicity == 0 ? 0 : toxicity / 10 + 1;
+        if (toxicityTier != updatedToxicityTier)
+        {
+            toxicityTier = updatedToxicityTier;
+            return true;
+        }
+        return false;
+    }
+
+    private void onToxicityChange(LivingEntity livingEntity)
     {
         Herblore.LOGGER.debug("Updating toxicity to be " + toxicity);
 
-        livingEntity.removePotionEffect(ModEffects.TOXICITY_RENDER.get());
-        int toxicityTier = getToxicityTier();
-        if (toxicityTier > 0)
+        if (updateToxicityTier())
         {
-            livingEntity.addPotionEffect(new EffectInstance(ModEffects.TOXICITY_RENDER.get(), Integer.MAX_VALUE, getToxicityTier(), false, false));
+            if (toxicityTier == 0)
+            {
+                livingEntity.removePotionEffect(ModEffects.TOXICITY_RENDER.get());
+            }
+            else
+            {
+                livingEntity.removePotionEffect(ModEffects.TOXICITY_RENDER.get());
+                livingEntity.addPotionEffect(new EffectInstance(ModEffects.TOXICITY_RENDER.get(), Integer.MAX_VALUE, toxicityTier - 1, false, false));
+            }
         }
     }
 
-    private int getToxicityTier()
+    private void tickToxicityDecay(LivingEntity livingEntity)
     {
-        return toxicity / 10;
+        if (toxicity > 0)
+        {
+            if (ticksSinceLastDecay == TOXICITY_DECAY_RATE)
+            {
+                removeToxicity(livingEntity, 1);
+                ticksSinceLastDecay = 0;
+            }
+            else
+            {
+                ticksSinceLastDecay++;
+            }
+        }
     }
 
     @Nullable
