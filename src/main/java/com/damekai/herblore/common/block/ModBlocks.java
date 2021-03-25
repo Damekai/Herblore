e66b830954152c4275a45c9799c5b79750565f8e
Damekai <damekaidev@gmail.com>
2021-03-24 23:33:30 -0400

    Fixed a bug with one of the reagent crops appearing as something else.

--- src/main/java/com/damekai/herblore/common/block/ModBlocks.java
+++ src/main/java/com/damekai/herblore/common/block/ModBlocks.java
@@ -26,7 +26,7 @@ public class ModBlocks
     public static final RegistryObject<BlockPerennialCrop> BREEZEBLOOM_CROP = BLOCKS.register("breezebloom_crop", () -> new BlockPerennialCrop(ModItems.BREEZEBLOOM.getSeeds()));
     public static final RegistryObject<BlockPerennialCrop> DESERTS_THIRST_CROP = BLOCKS.register("deserts_thirst_crop", () -> new BlockPerennialCrop(ModItems.DESERTS_THIRST.getSeeds()));
     public static final RegistryObject<BlockPerennialCrop> THUNDERSTAR_CROP = BLOCKS.register("thunderstar_crop", () -> new BlockPerennialCrop(ModItems.THUNDERSTAR.getSeeds()));
-    public static final RegistryObject<BlockPerennialCrop> SUNSTRIDERS_SIN_CROP = BLOCKS.register("sunstriders_sin_crop", () -> new BlockPerennialCrop(ModItems.SUNSPECKLE.getSeeds()));
+    public static final RegistryObject<BlockPerennialCrop> SUNSTRIDERS_SIN_CROP = BLOCKS.register("sunstriders_sin_crop", () -> new BlockPerennialCrop(ModItems.SUNSTRIDERS_SIN.getSeeds()));
     public static final RegistryObject<BlockPerennialCrop> SLAKEMOSS_CROP = BLOCKS.register("slakemoss_crop", () -> new BlockPerennialCrop(ModItems.SLAKEMOSS.getSeeds()));
     public static final RegistryObject<BlockPerennialCrop> SLAG_RIND_CROP = BLOCKS.register("slag_rind_crop", () -> new BlockPerennialCrop(ModItems.SLAG_RIND.getSeeds()));
     public static final RegistryObject<BlockPerennialCrop> VENGERVINE_CROP = BLOCKS.register("vengervine_crop", () -> new BlockPerennialCrop(ModItems.VENGERVINE.getSeeds()));
